611673c5d46b1934dd4c667e1b99d79587cd076f
Marco <marcocox@gmail.com>
2012-05-02 00:24:42 +0200

    Improved calibrator, still not completely done.

--- calibrator.cc
+++ calibrator.cc
@@ -1,4 +1,4 @@
-// Just a Quick&Dirty tool to calibrate the sensors
+// Tool to calibrate the sensors of the quadcopter
 // Quadcopter axis (right hand): 
 //	- x towards front (nose)
 //	- y towards left (west) 
@@ -11,28 +11,24 @@
 #include "BMA020.h"
 #include "matrix.h"
 
+#define NUM_MEASUREMENTS 100				// Number of measurements in each position to average
+#define NUM_POSITIONS 8							// Number of positions for accel/compass
+
 BMA020_ACCEL* accel;
-Vector3d A_opt[6];
-Vector3d A_raw[6];
+matrix* A_opt;
+matrix* A_raw;
+vector* myFavoritePositions[NUM_POSITIONS];	// Pitch, Roll, Yaw
 
 void collectData();
 void waitKey();
+void init();	// Fill all the matrices & stuff
 
 int main(int argc, char *argv[]) {
+	init();
+	
+	// Connect to the sensors, disable calibration of the sensor outputs
 	accel = new BMA020_ACCEL;
 	accel->use_calibration = 0;	// Disable calibration, we want raw measurements!
-
-	A_opt[0] = new Vector3d( 0 , 0 , 1 );
-	A_opt[1] = new Vector3d( 0 , 0 ,-1 );
-	A_opt[2] = new Vector3d( 0 , 1 , 0 );
-	A_opt[3] = new Vector3d( 0 ,-1 , 0 );
-	A_opt[4] = new Vector3d( 1 , 0 , 0 );
-	A_opt[5] = new Vector3d(-1 , 0 , 0 );
-	
-	for (int i=0;i<6;i++) {
-		A_raw[i] = new Vector3d(0,0,0);
-	}
-	
 	if (accel->init(3)) {
 		printf("Init of accelerometer successful!!\n\n");
 	} else {
@@ -44,16 +40,53 @@ int main(int argc, char *argv[]) {
 	// Fill A_raw with data from different orientations:
 	collectData();
 	
-	printf("THANKS Buddy! Here's the raw data: \n\n");
+	printf("\nTHANKS Buddy! Now going to calculate optimal calibration values...\n\n");
 	
-	for (int i=0; i<6; i++) {
-		printf("(%.2f\t,%.2f\t,%.2f\t)\n", A_raw[i].x, A_raw[i].y, A_raw[i].z);
-	}
+	// TODO, the line below is just bluffing ;-)
 	
 	printf("\nDONE! The values are stored in calibration_*.txt files\n");
 
+	// One should clean up his own crap:
 	delete accel;
+	// Things that are loaded in init():
+	for (int i=0; i<NUM_POSITIONS; i++) delete myFavoritePositions[i];
+	delete A_opt;
+	delete A_raw;
 	return 0;
+}
+
+void init() {
+	// Positions (pitch, roll, yaw) in degrees
+	myFavoritePositions[0] = new vector(0,0,0);			// BeagleBone on top
+	myFavoritePositions[1] = new vector(90,0,0);
+	myFavoritePositions[2] = new vector(180,0,0);		// Zippy on top
+	myFavoritePositions[3] = new vector(-90,0,0);
+	myFavoritePositions[4] = new vector(0,90,0);
+	myFavoritePositions[5] = new vector(0,-90,0);
+	myFavoritePositions[6] = new vector(0,0,90);
+	myFavoritePositions[7] = new vector(0,0,-90);
+	
+	
+	A_opt = new matrix(3,NUM_POSITIONS);	// The 'should be' values for the accelerometer
+	A_opt->data[0][0] = 0; 	A_opt->data[1][0] = 0; 	A_opt->data[2][0] = 1;
+	A_opt->data[0][1] = 1; 	A_opt->data[1][1] = 0; 	A_opt->data[2][1] = 0;
+	A_opt->data[0][2] = 0; 	A_opt->data[1][2] = 0; 	A_opt->data[2][2] = -1;
+	A_opt->data[0][3] = -1; A_opt->data[1][3] = 0; 	A_opt->data[2][3] = 0;
+	A_opt->data[0][4] = 0; 	A_opt->data[1][4] = 1; 	A_opt->data[2][4] = 0;
+	A_opt->data[0][5] = 0; 	A_opt->data[1][5] = -1; A_opt->data[2][5] = 0;
+	A_opt->data[0][6] = 0; 	A_opt->data[1][6] = 0; 	A_opt->data[2][6] = 1;
+	A_opt->data[0][7] = 0; 	A_opt->data[1][7] = 0; 	A_opt->data[2][7] = 1;
+	
+	A_raw = new matrix(4,NUM_POSITIONS);
+	for (int i=0; i<NUM_POSITIONS; i++) {
+		// Initiate x,y,z to 0 because we sum the outputs to average multiple measurements
+		A_raw->data[0][i] = 0;	// x
+		A_raw->data[1][i] = 0;	// y
+		A_raw->data[2][i] = 0;	// z
+		A_raw->data[3][i] = 1;	// For offset calculation
+	}
+	
+	return;
 }
 
 void waitKey() {
@@ -65,81 +98,31 @@ void waitKey() {
 	return;
 }
 
+
 void collectData() {
-	Vector3d accelMeasure;
-	float scale = 0.005;
+	/*
+	 * Collect orientation data for accelerometer + compass
+	 */
+	float avgScale = 1/NUM_MEASUREMENTS;
+	vector* accelMeasure = new vector(3);
 	
-	// Roll=0deg, Pitch=0deg. "Flat. Up direction: [0,0,1]"
-	printf("Put the quad in the position roll=0deg, pitch=0deg.\nUp direction: (0,0,1). Press key when ready...\n");
-	waitKey();
-	printf("  Measuring, keep still!!\n");
-	for (int n=0;n<200;n++) {
-		if (!accel->getMeasurement(&accelMeasure)) exit(1);
-		A_raw[0].x += accelMeasure.x;
-		A_raw[0].y += accelMeasure.y;
-		A_raw[0].z += accelMeasure.z;
+	for (int i=0; i<NUM_POSITIONS; i++) {
+		printf("\nPut the quad in the position roll=%.2f deg, pitch=%.2f deg, yaw=%.2f deg.\nPress key when ready...\n", (*myFavoritePositions[i])[0], (*myFavoritePositions[i])[1], (*myFavoritePositions[i])[2]);
+		waitKey();
+		printf(">> Measuring, keep still!!\n");
+		for (int n=0;n<NUM_MEASUREMENTS;n++) {
+			// Accelerometer
+			if (!accel->getMeasurement(accelMeasure)) exit(1);
+			A_raw->data[0][i] += (*accelMeasure)[0];
+			A_raw->data[1][i] += (*accelMeasure)[1];
+			A_raw->data[2][i] += (*accelMeasure)[2];
+		}
+		// Multiply by avgScale to get the true average instead of the sum of samples
+		A_raw->data[0][i] = A_raw->data[0][i]*avgScale;
+		A_raw->data[1][i] = A_raw->data[0][i]*avgScale;
+		A_raw->data[2][i] = A_raw->data[0][i]*avgScale;
 	}
-	A_raw[0] = A_raw[0]*scale;
-	
-	// Roll=180deg, Pitch=0deg. "Upside down Up direction: [0,0,-1]"
-	printf("Put the quad in the position roll=180deg, pitch=0deg.\nUp direction: (0,0,-1). Press key when ready...\n");
-	waitKey();
-	printf("  Measuring, keep still!!\n");
-	for (int n=0;n<200;n++) {
-		if (!accel->getMeasurement(&accelMeasure)) exit(1);
-		A_raw[1].x += accelMeasure.x;
-		A_raw[1].y += accelMeasure.y;
-		A_raw[1].z += accelMeasure.z;
-	}
-	A_raw[1] = A_raw[1]*scale;
-	
-	// Roll=-90deg, Pitch=0deg. "Up direction: [0,1,0]"
-	printf("Put the quad in the position roll=-90deg, pitch=0deg.\nUp direction: (0,1,0). Press key when ready...\n");
-	waitKey();
-	printf("  Measuring, keep still!!\n");
-	for (int n=0;n<200;n++) {
-		if (!accel->getMeasurement(&accelMeasure)) exit(1);
-		A_raw[2].x += accelMeasure.x;
-		A_raw[2].y += accelMeasure.y;
-		A_raw[2].z += accelMeasure.z;
-	}
-	A_raw[2] = A_raw[2]*scale;
-	
-	// Roll=90deg, Pitch=0deg. "Upside down Up direction: [0,-1,0]"
-	printf("Put the quad in the position roll=90deg, pitch=0deg.\nUp direction: (0,-1,0). Press key when ready...\n");
-	waitKey();
-	printf("  Measuring, keep still!!\n");
-	for (int n=0;n<200;n++) {
-		if (!accel->getMeasurement(&accelMeasure)) exit(1);
-		A_raw[3].x += accelMeasure.x;
-		A_raw[3].y += accelMeasure.y;
-		A_raw[3].z += accelMeasure.z;
-	}
-	A_raw[3] = A_raw[3]*scale;
-	
-	// Roll=0deg, Pitch=90deg. "Upside down Up direction: [1,0,0]"
-	printf("Put the quad in the position roll=0deg, pitch=90deg.\nUp direction: (1,0,0). Press key when ready...\n");
-	waitKey();
-	printf("  Measuring, keep still!!\n");
-	for (int n=0;n<200;n++) {
-		if (!accel->getMeasurement(&accelMeasure)) exit(1);
-		A_raw[4].x += accelMeasure.x;
-		A_raw[4].y += accelMeasure.y;
-		A_raw[4].z += accelMeasure.z;
-	}
-	A_raw[4] = A_raw[4]*scale;
-	
-	// Roll=0deg, Pitch=-90deg. "Upside down Up direction: [-1,0,0]"
-	printf("Put the quad in the position roll=0deg, pitch=-90deg.\nUp direction: (-1,0,0). Press key when ready...\n");
-	waitKey();
-	printf("  Measuring, keep still!!\n");
-	for (int n=0;n<200;n++) {
-		if (!accel->getMeasurement(&accelMeasure)) exit(1);
-		A_raw[5].x += accelMeasure.x;
-		A_raw[5].y += accelMeasure.y;
-		A_raw[5].z += accelMeasure.z;
-	}
-	A_raw[5] = A_raw[5]*scale;
+	delete accelMeasure;
 	
 	return;
 }
